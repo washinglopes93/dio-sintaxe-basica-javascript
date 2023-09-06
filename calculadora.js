71e5dbb519b605aba92b8f0168614afc13894b31
Washington <washinglopes@hotmail.com>
2023-09-06 17:11:49 -0300

    Funcao e Calculadora

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+//tipos de funções
+
+//declaraticas
+/* function funcao(){
+    console.log('mensagem')
+}
+funcao(); */
+
+//expressões de funções
+var funcao = function (){
+    console.log('Sou mensagem de função de expressão');
+}
+funcao();
---
+++ funcao.js
@@ -0,0 +1,11 @@
+/* //Função simples
+function funcao(){
+    console.log("Alguma mensagem")
+}
+funcao(); */
+
+//Função com parametros
+function mensagem(pri, seg){
+    console.log(pri, seg);
+}
+mensagem('primeira mensagem', 'segunda mensagem');
