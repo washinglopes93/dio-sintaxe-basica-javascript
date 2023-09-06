09537074bc5e148fb980b0ed02201e92eaa1e453
Washington <washinglopes@hotmail.com>
2023-09-06 11:23:14 -0300

    Variavel Concluido

--- variavel.js
+++ variavel.js
@@ -1,5 +1,84 @@
-//Tipos primitivos
+//TIPOS PRIMITIVOS
 
 //boolean teste tipo de dados verdadeiro ou falso
-var trueorfalse = false;
+
+/*var trueorfalse = false;
 console.log(typeof(trueorfalse));
+
+//number
+var anynum = 1;
+console.log(typeof(anynum));
+
+//string
+var nome = "eu";
+console.log(typeof(nome)); */
+
+//COMO DECLARAR VARIAVEIS
+
+/*var variavel = 'algumacoisa';
+console.log(variavel);
+
+let varlet = 'diana';
+varlet ='martine' //Permite alterar o valor da variavel depois da sua criação
+console.log(varlet);
+
+const constante = 'juju';
+console.log(constante); */
+
+/*var escopoglobal = 'global';
+console.log(escopoglobal);
+
+function escopolocal(){
+    let escopolocalInterno = 'local'; //por conveção utiliazamos escopo local
+    console.log(escopolocalInterno);
+}
+
+escopolocal(); */
+
+//ATRIBUIÇÃO
+/*var atribuição = 'washington';
+//Comparação 
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica); */
+
+//OPERADORES ARITMÉTICOS
+//adição
+/*var adicao = 1+1;
+console.log(adicao);
+var subtracao = 2-1;
+console.log(subtracao);
+var mult = 2*2;
+console.log(mult);
+var divreal = 6/2;
+console.log(divreal);
+var divrest = 5%2;
+console.log(divrest);
+var pot = 2 ** 10;
+console.log(pot);*/
+
+//OPERADORES RELACIONAIS
+/* var maiorque = 5 > 2;
+console.log(maiorque);
+var menorque = 5 < 2;
+console.log(menorque);
+var maiorigualque = 5>=2;
+console.log(maiorigualque);
+var menorigualque = 5<=5;
+console.log(menorigualque); */
+
+//OPERADORES LÓGICOS
+var AndOne = true && true;
+console.log(AndOne);
+var AndTwo = true && false;
+console.log(AndTwo)
+var OrOne = true || true;
+console.log(OrOne);
+var OrTwo = true || false;
+console.log(OrTwo);
+var verdade = !true;
+console.log(verdade);
+var falso = !false;
+console.log(falso);
